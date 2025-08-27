d0045fb5c094f9a6d6e6993505db95c7ca055a25
Souvik Mandal <riju.souvik@gmail.com>
2025-08-28 01:30:15 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project - Medical Chatbot',
+    version= '0.0.0',
+    author= 'Souvik Mandal',
+    author_email= 'riju.souvik@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
